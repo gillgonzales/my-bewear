bdce55901d4ef28869f4a890ab61e165b4e9737b
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-08-05 23:58:07 -0300

    refactor: add and config code sort tools ( prettier and eslint). npm i -D prettier@3.6.2 prettier-plugin-tailwindcss@0.6.14; npm install --save-dev eslint-plugin-simple-import-sort@12.1.1

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
+
+import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
     <>
-      <h1 className="font-bold text-blue-500 text-lg">Products</h1>;
-      <Button>Click me</Button>
+      <h1 className="text-lg font-bold text-blue-500">Products</h1>;
+      <Button className="bg-blue-500 px-4 py-3 text-white">Click me</Button>
     </>
   );
 };
