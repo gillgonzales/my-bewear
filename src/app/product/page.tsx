333ab20304884e2300847c0de564240eedbd6047
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-08-05 23:30:23 -0300

    feat: aula 01 intro next e shadcn ui
    npx create-next-app@15.4.1 my-bewear; npx shadcn@2.9.2 init; npx shadcn@2.9.2 add button

---
+++ src/app/product/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <h1 className="font-bold text-blue-500 text-lg">Products</h1>;
+      <Button>Click me</Button>
+    </>
+  );
+};
+
+export default page;
