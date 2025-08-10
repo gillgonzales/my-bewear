4232f585dc79af1899e635892bb28860cf2b2573
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-08-09 21:36:19 -0300

    chore: organize code and remove useless things

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 const page = () => {
   return (
     <>
-      <h1 className="text-lg font-bold text-blue-500">Products</h1>;
+      <h1 className="text-lg font-bold text-blue-500">Products</h1>
       <Button className="px-4 py-3 text-white">Click me</Button>
     </>
   );
